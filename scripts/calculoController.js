3db263ced0ecf93200c5296262baef04cd36d8c5
Ferres Declair <joan.ferres@external.wolterskluwer.com>
2017-04-20 14:55:45 +0200

    corrección en calculoController

--- scripts/calculoController.js
+++ scripts/calculoController.js
@@ -1,17 +1,17 @@
 define([
 
-        ], function() {
-            return function(app) {
-                function controller($scope, modelService) {
-                    var vm = this;
-                    vm.saludo = "hola mundo";
-                    vm.sexo = "M";
-                    vm.edad = "25";
+], function() {
+    return function(app) {
+        function controller($scope, modelService) {
+            var vm = this;
+            vm.saludo = "hola mundo";
+            vm.sexo = "M";
+            vm.edad = "25";
 
-                    modelService.initialize();
-                };
+            modelService.initialize();
+        };
 
-                controller.$inject = ["$scope", "modelService
-                    app.controller('calculoController', controller);
-                };
-            });
+        controller.$inject = ["$scope", "modelService"];
+        app.controller('calculoController', controller);
+    };
+});
